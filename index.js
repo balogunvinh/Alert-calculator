920db5fbf342e7c8cd108d57060b30d77455beb7
Balogunvinh <balogunvinh@gmail.com>
2022-06-24 04:17:24 +0100

    javascript alert calculator

---
+++ index.js
@@ -0,0 +1,19 @@
+const operator = prompt("Enter operator ( either +, -, * or /):");
+
+const number1 = parseFloat(prompt("enter any number"));
+
+const number2 = parseFloat(prompt("enter your second number"));
+
+let result;
+
+if (operator == "+") {
+  result = number1 + number2;
+} else if (operator == "-") {
+  result = number1 - number2;
+} else if (operator == "*") {
+  result = number1 * number2;
+} else if(operator == "/"){
+  result = number1 / number2;
+}
+
+console.log(`${number1} ${operator} ${number2} = ${result}`);
